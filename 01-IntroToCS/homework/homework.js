768c4c1f948731a123e3a72c7737b93d6f6d5bd4
CristianMorera <cristianmoreradiaz94@gmail.com>
2022-09-13 08:00:29 -0500

    version 1.0.01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,22 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  var Decimal = 0;
+  for (let i = 0; i < num.length; i++) {
+    Decimal = (num[i] * Math.pow(2, num.length -1 -i)) + Decimal ;   
+  }
+  return Decimal;
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  var binario=[];
+  while (num!==0){
+    binario= (num%2)+binario;
+    num= Math.floor(num / 2);
+    }
+  return binario;
 }
 
 
